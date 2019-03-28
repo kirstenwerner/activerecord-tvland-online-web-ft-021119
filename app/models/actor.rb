ef5d2fb588035c8e2405d9826acc7792b724a09e
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-28 00:56:47 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,13 @@ class Actor < ActiveRecord::Base
   has_many :shows through: :characters 
   
   def full_name 
-    self.first_name
+    full_name = []
+    full name << self.first_name
+    full name << self.last_name 
+    full_name.join(" ")
+  end 
+  
+  def list_roles 
+    self.characters
   end 
 end
